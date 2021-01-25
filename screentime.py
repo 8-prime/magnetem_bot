9d31514cd51d3a15e8740877535a1ee058aa259b
Stefan Hoffmeister <hoffmeister.stefan99@gmail.com>
2021-01-25 14:59:27 +0100

    testing in screentime to understadn d3d better starting work on implementation using d3d rather than pyautogui for speeedd reasons. hopefully d3d with np will be hella faster

--- screentime.py
+++ screentime.py
@@ -2,6 +2,6 @@ import d3dshot
 import numpy as np
 d = d3dshot.create(capture_output="numpy")
 
-d.screenshot()
-
-print(np.shape(d.get_latest_frame()))
+image = d.screenshot()
+print("Doing somethgin")
+print(image[1000,1000])
